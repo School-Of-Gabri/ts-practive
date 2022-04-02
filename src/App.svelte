2fcfb63aedf47492dc556415c65ec036aab536f7
Gabri Botha <AngelODeath@outlook.com>
2022-04-02 11:33:53 +0200

    Add types to App.svelte js code

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,10 @@ import {onMount} from "svelte"
 
 import {Pixi, Text} from "svelte-pixi"
 
-import Greeter from "./lib/Greeter"
+import {Greeter} from "./lib/Greeter"
 import {GameApp as Game} from "./game"
 
-let name, my_g_app, my_pixi_game
+let name: string, my_g_app: Greeter, my_pixi_game: Game
 
 function set_name(): void {
     name = "Gabri Botha"
@@ -15,10 +15,14 @@ function set_name(): void {
 onMount(async () => {
     name = "Tom Smith"
 
-    my_g_app = new Greeter(document.getElementById("greeter_section"))
+    my_g_app = new Greeter(document.getElementById("greeter_section") as HTMLElement)
     console.log("Greeter has been created!", my_g_app)
 
-    my_pixi_game = new Game(document.getElementById("game_section"), 640, 480)
+    my_pixi_game = new Game(
+        document.getElementById("game_section") as HTMLElement,
+        640,
+        480
+    )
     console.log("Game has been created!", my_pixi_game)
 })
 </script>
