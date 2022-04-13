a9d9dcbed25a88d2c8595ed7f367a56ffdb4ad42
Gabri Botha <AngelODeath@outlook.com>
2022-04-13 18:46:00 +0200

    Move and fixup vite.config.ts to vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import {defineConfig} from "vite"
+import {svelte} from "@sveltejs/vite-plugin-svelte"
+
+import sveltePreprocess from "svelte-preprocess"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [svelte({preprocess: sveltePreprocess()})],
+    publicDir: "./assets/",
+    build: {
+        outDir: "./dist/",
+    },
+})
